e953859fdacb8e9fbf6106c7ce028dbecb023c1a
tonybwatson <tonybwatson@gmail.com>
2021-11-11 19:45:08 -0500

    portfolio page style updates

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import bouncing from '../img/bouncing.jpeg'
 import clock from '../img/clock.jpeg'
 import login from '../img/login.jpeg'
 import mindreader from '../img/mindreader.jpeg'
@@ -10,17 +9,20 @@ import { Card, CardGroup, Button, Container, Row, Col } from 'react-bootstrap'
 
 export default function Portfolio() {
 	return (
-		<div>
 			<>
-				<Container>
-						<CardGroup>
-					<Row>
+			<Container>
+				<Row className="my-5">
+			<h1 className="py-2 text-center text-info border border-info"><u>Projects</u></h1>
+				</Row>
+
+					<CardGroup>
+						<Row>
 							<Col className="col-md-4 mb-3">
-								<Card className="bg-dark border border-info mb-4 h-100">
+								<Card className="bg-dark border border-info mb-4 h-100 animate__animated animate__zoomIn">
 									<Card.Body>
 										<Card.Title>Login Page</Card.Title>
 										<img src={login} alt="login screen" className="img-fluid" />
-										<Card.Text>
+										<Card.Text className="mt-3">
 											This project is a login page which utilizes React hooks to allow users to set up a user account, log in, and log out.
     								</Card.Text>
 									</Card.Body>
@@ -36,12 +38,11 @@ export default function Portfolio() {
 							</Col>
 
 							<Col className="col-md-4 mb-3 ">
-								<Card className="bg-dark border border-info mb-4 h-100">
-
+								<Card className="bg-dark border border-info mb-4 h-100 animate__animated animate__zoomIn">
 									<Card.Body>
 										<Card.Title>To-Do List</Card.Title>
 										<img src={todo} alt="to-do list" className="img-fluid" />
-										<Card.Text>
+										 <Card.Text className="mt-3">
 											The objective of this project was to create a to-do list using React. New to-do items can be added to the list, checked off and deleted.
     								</Card.Text>
 									</Card.Body>
@@ -57,11 +58,11 @@ export default function Portfolio() {
 							</Col>
 
 							<Col className="col-md-4 mb-3 ">
-								<Card className="bg-dark border border-info mb-4 h-100">
+								<Card className="bg-dark border border-info mb-4 h-100 animate__animated animate__zoomIn">
 									<Card.Body>
 										<Card.Title>Restaurant Menu</Card.Title>
 										<img src={restaurant} alt="restaurant menu" className="img-fluid" />
-										<Card.Text>
+										 <Card.Text className="mt-3">
 											This project uses an Axios API call and React to dynamically render a restaurant menu.
     								</Card.Text>
 									</Card.Body>
@@ -75,136 +76,71 @@ export default function Portfolio() {
 									</Card.Footer>
 								</Card>
 							</Col>
-						
-					</Row>
+						</Row>
 
-					<Row>
-						<Col className="col-md-4 mb-3 ">
-							<Card className="bg-dark border border-info mb-4 h-100">
-								<Card.Body>
-									<Card.Title>Mind Reader</Card.Title>
-									<Card.Text>
-										This project uses Javascript to control page state. It uses a simple mathematical trick to guess the user's chosen number.
+						<Row>
+							<Col className="col-md-4 mb-3 ">
+								<Card className="bg-dark border border-info mb-4 h-100 animate__animated animate__zoomIn">
+									<Card.Body>
+										<Card.Title>Mind Reader</Card.Title>
+										<img src={mindreader} alt="mindreader" className="img-fluid" />
+										 <Card.Text className="mt-3">
+											This project uses Javascript to control page state. It uses a simple mathematical trick to guess the user's chosen number.
     								</Card.Text>
-								</Card.Body>
-								<Card.Footer>
-									<Button className="btn btn-dark border border-info m-1"
-										href=""
-										target="_blank">Project</Button>
-									<Button className="btn btn-dark border border-info m-1"
-										href=""
-										target="_blank">Code</Button>
-								</Card.Footer>
-							</Card>
-						</Col>
+									</Card.Body>
+									<Card.Footer>
+										<Button className="btn btn-dark border border-info m-1"
+											href="https://tonybwatson.github.io/mindReader/"
+											target="_blank">Project</Button>
+										<Button className="btn btn-dark border border-info m-1"
+											href="https://github.com/tonybwatson/mindReader"
+											target="_blank">Code</Button>
+									</Card.Footer>
+								</Card>
+							</Col>
 
-						<Col className="col-md-4 mb-3 ">
-							<Card className="bg-dark border border-info mb-4 h-100">
-								<Card.Body>
-									<Card.Title>Alarm Clock</Card.Title>
-									<Card.Text>
-										Some quick example text to build on the card title and make up the bulk of
-										the card's content.
+							<Col className="col-md-4 mb-3 ">
+								<Card className="bg-dark border border-info mb-4 h-100 animate__animated animate__zoomIn">
+									<Card.Body>
+										<Card.Title>Weather App</Card.Title>
+										<img src={weather} alt="weather app" className="img-fluid" />
+										 <Card.Text className="mt-3">
+											This project utilizes an API call to display weather information from a user-submitted zip code.
     								</Card.Text>
-								</Card.Body>
-								<Card.Footer>
-									<Button className="btn btn-dark border border-info m-1"
-										href=""
-										target="_blank">Project</Button>
-									<Button className="btn btn-dark border border-info m-1"
-										href=""
-										target="_blank">Code</Button>
-								</Card.Footer>
-							</Card>
-						</Col>
+									</Card.Body>
+									<Card.Footer>
+										<Button className="btn btn-dark border border-info m-1"
+											href="https://tonybwatson.github.io/weather_app/"
+											target="_blank">Project</Button>
+										<Button className="btn btn-dark border border-info m-1"
+											href="https://github.com/tonybwatson/weather_app"
+											target="_blank">Code</Button>
+									</Card.Footer>
+								</Card>
+							</Col>
 
-						<Col className="col-md-4 mb-3 ">
-							<Card className="bg-dark border border-info mb-4 h-100">
-								<Card.Body>
-									<Card.Title>Alarm Clock</Card.Title>
-									<Card.Text>
-										Some quick example text to build on the card title and make up the bulk of
-										the card's content.
-    								</Card.Text>
-								</Card.Body>
-								<Card.Footer>
-									<Button className="btn btn-dark border border-info m-1"
-										href=""
-										target="_blank">Project</Button>
-									<Button className="btn btn-dark border border-info m-1"
-										href=""
-										target="_blank">Code</Button>
-								</Card.Footer>
-							</Card>
-						</Col>
-					</Row>
-
-					<Row>
-						<Col className="col-md-4 mb-3 ">
-							<Card className="bg-dark border border-info mb-4 h-100">
-								<Card.Body>
-									<Card.Title>Alarm Clock</Card.Title>
-									<Card.Text>
-										Some quick example text to build on the card title and make up the bulk of
-										the card's content.
+							<Col className="col-md-4 mb-3 ">
+								<Card className="bg-dark border border-info mb-4 h-100 animate__animated animate__zoomIn">
+									<Card.Body>
+										<Card.Title>80's Alarm Clock</Card.Title>
+										<img src={clock} alt="clock" className="img-fluid" />
+										 <Card.Text className="mt-3">
+											This project used vanilla Javascript and a ton of CSS to make an alarm clock.
     							</Card.Text>
-								</Card.Body>
-								<Card.Footer>
-									<Button className="btn btn-dark border border-info m-1"
-										href=""
-										target="_blank">Project</Button>
-									<Button className="btn btn-dark border border-info m-1"
-										href=""
-										target="_blank">Code</Button>
-								</Card.Footer>
-							</Card>
-						</Col>
-
-						<Col className="col-md-4 mb-3 ">
-							<Card className="bg-dark border border-info mb-4 h-100">
-
-								<Card.Body>
-									<Card.Title>Alarm Clock</Card.Title>
-									<Card.Text>
-										Some quick example text to build on the card title and make up the bulk of
-										the card's content.
-    							</Card.Text>
-								</Card.Body>
-								<Card.Footer>
-									<Button className="btn btn-dark border border-info m-1"
-										href=""
-										target="_blank">Project</Button>
-									<Button className="btn btn-dark border border-info m-1"
-										href=""
-										target="_blank">Code</Button>
-								</Card.Footer>
-							</Card>
-						</Col>
-
-						<Col className="col-md-4 mb-3 ">
-							<Card className="bg-dark border border-info mb-4 h-100">
-
-								<Card.Body>
-									<Card.Title>Alarm Clock</Card.Title>
-									<Card.Text>
-										Some quick example text to build on the card title and make up the bulk of
-										the card's content.
-    							</Card.Text>
-								</Card.Body>
-								<Card.Footer>
-									<Button className="btn btn-dark border border-info m-1"
-										href=""
-										target="_blank">Project</Button>
-									<Button className="btn btn-dark border border-info"
-										href=""
-										target="_blank">Code</Button>
-								</Card.Footer>
-							</Card>
-						</Col>
-					</Row>
+									</Card.Body>
+									<Card.Footer>
+										<Button className="btn btn-dark border border-info m-1"
+											href="https://tonybwatson.github.io/alarm_clock/"
+											target="_blank">Project</Button>
+										<Button className="btn btn-dark border border-info m-1"
+											href="https://github.com/tonybwatson/alarm_clock"
+											target="_blank">Code</Button>
+									</Card.Footer>
+								</Card>
+							</Col>
+						</Row>
 					</CardGroup>
 				</Container>
 			</>
-		</div>
 	)
 }
