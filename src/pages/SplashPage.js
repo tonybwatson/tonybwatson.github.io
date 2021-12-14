7fc735582f3248b67b6c4c35750c08d95b92c4c1
tonybwatson <tonybwatson@gmail.com>
2021-12-13 21:32:51 -0500

    change splash page pic

--- src/pages/SplashPage.js
+++ src/pages/SplashPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import facepic from '../img/cuba-pic-square.jpg'
+import facepic from '../img/squareportrait.png'
 
 export default function SplashPage() {
 	return (
@@ -14,14 +14,14 @@ export default function SplashPage() {
 				rounded 
 				mt-4 animate__animated animate__fadeIn animate__delay-1s">
 				<div className="row">
-					<h1 className="my-4"><strong>Hi.</strong></h1>
+					<h1 className="my-2"><strong>Hi.</strong></h1>
 					<h2>My name is </h2>
 					<h1><strong><u>Tony Watson</u></strong>.</h1>
 				</div>
 				<div className="row">
 					<h2>I'm a web developer.</h2>
 				</div>
-				<img src={facepic} alt="Me" className="img-fluid rounded-circle my-5" />
+				<img src={facepic} alt="Me" className="img-fluid rounded-circle my-3" id="facepic"/>
 				<h1 className="mb-4"><u>While you're here:</u> </h1>
 				<h2>Learn a little bit <a href="/about" className="link-light">about me</a>.</h2>
 				<h2>Have a look at my <a href="/portfolio" className="link-light">portfolio</a>.</h2>
